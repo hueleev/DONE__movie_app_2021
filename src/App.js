a9ccc38ddbb5c16aaf89cf18067a4d455934c2a5
Hyungwon Lee <hueleev@gmail.com>
2022-05-09 10:27:24 +0900

    end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Detail from './routes/Detail';
 import Home from './routes/Home';
 function App() {
 	return (
 		<Router>
 			<Switch>
-				<Route path="/movie">
+				<Route path="/movie/:id">
 					<Detail />
 				</Route>
 				<Route path="/">
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './Detail.module.css';
+
 function Detail() {
-	return <h1>Detail</h1>;
+	const { id } = useParams();
+	const [movie, setMoive] = useState({});
+	const getMovie = async () => {
+		const json = await (
+			await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+		).json();
+		setMoive(json.data.movie);
+		console.log(json.data.movie);
+	};
+	{
+		movie.genres.map(g => <li key={g}>{g}</li>);
+	}
+	useEffect(() => {
+		getMovie();
+	}, []);
+
+	return (
+		<div>
+			<div className={styles.movie}>
+				<div>
+					<img className={styles.movie__img} alt={movie.id} src={movie.medium_cover_image} />
+				</div>
+				<div>
+					<h1 className={styles.movie__title}>{movie.title}</h1>
+					<h4 className={styles.moive__year}>{movie.year}</h4>
+					<ul className={styles.movie__genres}>
+						{movie.genres.map(v => (
+							<li key={v}>{v}</li>
+						))}
+					</ul>
+					<p>{movie.description_full}</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Detail;
